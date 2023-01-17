425f269c241301da2be1b4899a68655dd43bbb8c
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-01-17 17:56:32 +0300

    tasks 0.15

--- 0.15 Number/11/app.js
+++ 0.15 Number/11/app.js
@@ -3,3 +3,7 @@
 // • Вырежите из нее 2 и 3 слова.
 // • Найдите индекс 2 подстроки
 
+let str = 'jsdgfka kajsdgfk alsjdf';
+console.log(str.length);
+console.log(str.slice(8,22));
+console.log(str.slice(0,7));
--- 0.15 Number/18/app.js
+++ 0.15 Number/18/app.js
@@ -1,2 +1,7 @@
 // Дан массив. Вывести каждый отдельный элемент в консоль (for)
 
+let array = ['1', '2', '4', '8', '9'];
+for (let i = 0; i < array.length; i++) {
+    const a = array[2];
+    console.log(a);
+}

3a1434f4127066eedcf4b04b23d05593b76ed047
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-01-22 03:50:04 +0300

    tasks

--- 0.17 for/12/app.js
+++ 0.17 for/12/app.js
@@ -1,11 +1,31 @@
 // 12. Найдите минимальное значение в массиве используя цикл
 
-const arr = [25, 95, 456, 1];
-let min = arr[0];
+const ar = [5, 10, -1, 359];
+let res = [0];
 
-for (i = 0; i < arr.length; i++) {
-   if (arr[i] < min) {
-      min = arr[i]
+for (let i = 0; i < ar.length; i++){
+   if (ar[i] < res ) {
+      res = ar[i]
    }
 }
-console.log(min);
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [25, 95, 456, 1];
+// let min = arr[0];
+
+// for (i = 0; i < arr.length; i++) {
+//    if (arr[i] < min) {
+//       min = arr[i]
+//    }
+// }
+// console.log(min);

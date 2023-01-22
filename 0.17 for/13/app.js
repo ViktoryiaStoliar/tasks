d649bcbcecd6d1fc37526fb9a98e0b9dbb005f5b
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-01-22 03:24:14 +0300

    task 18-13

--- 0.17 for/13/app.js
+++ 0.17 for/13/app.js
@@ -1,10 +1,28 @@
 // 13. Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение
 // элементов этого массива
 
-const arr = [2, 3, 4, 5];
-let res = arr[0];
-
-for (i = 0; i < arr.length; i++) {
-   res *= arr[i]
+const ar = [2, 3, 4, 5];
+let res = ar[0];
+for (i = 1; i < ar.length; i++){
+   res *= ar[i];
 }
 console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [2, 3, 4, 5];
+// let res = arr[0];
+
+// for (i = 0; i < arr.length; i++) {
+//    res *= arr[i]
+// }
+// console.log(res);
--- 0.17 for/18/app.js
+++ 0.17 for/18/app.js
@@ -3,14 +3,14 @@
 // 1234 -> 1 неч 2 чет 3 неч 4 чет
 // 444444 -> 4 чет 4 чет 4 чет 4 чет 4 чет 4 чет
 
-const str = `123405`;
-let res = ``;
-
-for (i = 0; i < str.length; i++){
-   if(str[i] % 2 === 0) {
-      res += str[i] + ` чет `
+const num = "456978210";
+let res = " ";
+for (let i = 0; i < num.length; i++) {
+   if (num[i] % 2 === 0) {
+      res += num[i] + ` - четн.; `
    } else {
-      res += str[i] +` нечет `
+      res += num[i] + `  - нечетн.; `
    }
 }
 console.log(res);
+

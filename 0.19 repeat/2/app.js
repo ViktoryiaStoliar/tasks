3f76617fa668ddcb084c967a0ba3edfe89f29a2f
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-01-24 21:00:50 +0300

    repeat

---
+++ 0.19 repeat/2/app.js
@@ -0,0 +1,14 @@
+// 2. Реализовать строку, в которую 5 раз будет конкатенироваться новое значение
+// prompt (где значение число)
+
+
+
+let res = '';
+for (let i = 0; i < 5; i++){
+    let newValue = prompt();
+
+    if(isNaN(newValue)){
+        re += newValue
+    }
+}
+console.log(res);
---
+++ 0.19 repeat/6/app.js
@@ -0,0 +1,14 @@
+// 7. На входе строка. Необходимо реализовать алгоритм, убирающий дублирующие
+// символы в строке
+// hschoolofficial -> hscolfia
+
+let str = `hschoolofficial`;
+let res = ``;
+
+for (let i = 0; i < str.length; i++){
+    if (!res.includes(str[i])){
+    res += str[i]
+    }
+}
+console.log(res);
+

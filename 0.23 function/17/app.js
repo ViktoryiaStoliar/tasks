6084b45c0029b9e9185f76a802d0c86ebfadd991
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-02-11 23:49:32 +0300

    task17

--- 0.23 function/17/app.js
+++ 0.23 function/17/app.js
@@ -6,19 +6,17 @@ const str = 'hschool'.split('');
 console.log(str);
 
 function isValid(str_) {
-    let result = [];
-    for (let i = 0; i < str_.length; i++){
-        if (str_[i] % 2 == 0) {
-            result.push(str_[i].toUpperCase())
-        } else {
-            result.push(str_[i].toLowerCase())
-        }
-    }
+    let result = str_.map(function(el, i){
+        if(i % 2 ==0){
+            return el.toLowerCase()
+        } else return el.toUpperCase()
+    })
+    
 return result
 }
 
 
-const result = isValid(str);
+let result = isValid(str);
 console.log(result.join(''));
 
 

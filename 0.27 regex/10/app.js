50806d517c78c12d09da4443f0f89e7e78254160
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-02-26 00:06:17 +0300

    regex

--- 0.27 regex/10/app.js
+++ 0.27 regex/10/app.js
@@ -3,3 +3,27 @@
 // Вырезая куски кода вы поняли, что их чересчур много. Необходимо написать
 // регулярное выражение, которое вырежет все комментарии из html разметки
 // <!-- -->
+
+const data = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    //<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    //<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="./app.js"></script>
+</body>
+</html>`
+
+function checkHTMLFiles(s) {
+    try {
+        let result = s.replace(/\/\/<[^>]*>/gm, ' ');
+        return result
+    } catch (error) {
+        return error.message
+    }
+}
+let result = checkHTMLFiles(data);
+console.log(result);

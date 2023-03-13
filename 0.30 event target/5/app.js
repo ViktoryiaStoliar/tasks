fb32ca882852199f6b9420ebcb503aef173c20a2
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-03-13 22:48:08 +0300

    repeat

--- 0.30 event target/5/app.js
+++ 0.30 event target/5/app.js
@@ -1,13 +1,17 @@
 // Добавьте картинку на страницу. При 
 // наведении на картинку мышкой необходимо ее 
 // изменять на другую картинку. Но как только мышка
-//  снова отходит в сторону, 
+// снова отходит в сторону, 
 // то возвращается первая картинка
 
 const img=document.querySelector("img");
 
 img.addEventListener('mouseover', function(){
+    
     img.src='./assets/HS (11).png'
 });
 
-img.add
+img.addEventListener('mouseout', function(){
+
+    img.src='./assets/HS (10).png'
+})

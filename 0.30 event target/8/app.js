370fd30fb59f3e38c347c4a1de42b2bc18e76288
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-04-09 22:43:20 +0300

    repeat

--- 0.30 event target/8/app.js
+++ 0.30 event target/8/app.js
@@ -1,5 +1,9 @@
-document.createElement('div')
-document.createElement('button').addEventListener('click', () => {
-    console.log('+');
+// document.createElement('div')
+// document.createElement('button').addEventListener('click', () => {
+//     console.log('+');
+// }
+
+function test() {
+    console.log(this);
 }
-)
+test()
--- 0.38 repeat js/13/app.js
+++ 0.38 repeat js/13/app.js
@@ -1,76 +1,77 @@
-// // 13. Реализуйте класс Calculator, который будет содержать метод doSqrt,
-// // возвращающий корень числа принимаемого в параметрах. 
-
-// // {
-// //     class Calculator {
-// //         n;
-// //         setN(n) {
-// //             this.n = n
-// //         }
-
-// //         doSqrt() {
-// //             const sqrt = Math.sqrt(this.n);
-// //             return sqrt
-// //         }
-// //     }
-
-// //     const calculator = new Calculator();
-// //     calculator.setN(9)
-// //     const res = calculator.doSqrt();
-// //     console.log(res);
-// // }
+// 13. Реализуйте класс Calculator, который будет содержать метод doSqrt,
+// возвращающий корень числа принимаемого в параметрах. 
 
 // {
 //     class Calculator {
-//         doSqrt(a) {
-//             console.log(Math.sqrt(a));
+//         n;
+//         setN(n) {
+//             this.n = n
 //         }
-//     }
-//     const calculator = new Calculator();
-//     calculator.doSqrt(9);
-// }
 
-// {
-//     class Calculator {
-//         constructor(a) {
-//             this.a = a
-//         }
 //         doSqrt() {
-//             console.log(Math.sqrt(this.a));
+//             const sqrt = Math.sqrt(this.n);
+//             return sqrt
 //         }
-
 //     }
-//     const calculator = new Calculator(9);
-//     calculator.doSqrt();
+
+//     const calculator = new Calculator();
+//     calculator.setN(9)
+//     const res = calculator.doSqrt();
+//     console.log(res);
 // }
 
-// {class Calculator {
-//     a;
-//     setA(a) {
-//         this.a = a;
-//     }
-//     getA(){
-//         return this.a
-//     }
-//     doSqrt(){
-//         console.log(Math.sqrt(this.a));
-//     }
-// }
+{
+    class Calculator {
+        doSqrt(a) {
+            console.log(Math.sqrt(a));
+        }
+    }
+    const calculator = new Calculator();
+    calculator.doSqrt(9);
+}
 
-// const calculator = new Calculator();
-// calculator.setA(9);
-// calculator.doSqrt()};
+{
+    class Calculator {
+        constructor(a) {
+            this.a = a
+        }
+        doSqrt() {
+            console.log(Math.sqrt(this.a));
+        }
 
+    }
+    const calculator = new Calculator(9);
+    calculator.doSqrt();
+}
 
-class OOP {
-    arr = [1,2,3]
-    doSum(){
-        const sum = this.arr.reduce(function(sum, el){
-            return sum+el
-        })
-        return sum
+{class Calculator {
+    a;
+    setA(a) {
+        this.a = a;
+    }
+    getA(){
+        return this.a
+    }
+    doSqrt(){
+        console.log(Math.sqrt(this.a));
     }
 }
-const oop = new OOP();
-const res = oop.doSum();
-console.log(res);
+
+const calculator = new Calculator();
+calculator.setA(9);
+calculator.doSqrt()};
+
+
+// class OOP {
+//     arr = [1,2,3]
+//     doSum(){
+//         const sum = this.arr.reduce(function(sum, el){
+//             return sum+el
+//         })
+//         return sum
+//     }
+// }
+// const oop = new OOP();
+// const res = oop.doSum();
+// console.log(res);
+

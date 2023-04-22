73bb26059aeb218d8de56e122696dc7bc56eac2b
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-04-22 23:53:12 +0300

    sql

---
+++ sql/app.js
@@ -0,0 +1,9 @@
+// use cinema;
+// select * from movies;
+// -- 10
+// select avg(length_minutes), director
+// from movies
+// group by director;
+// -- 11 айти все фильмы режиссера начинающегося на “John”
+// select title
+// from movies

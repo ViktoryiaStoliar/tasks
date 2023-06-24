a0a55b176637386976798412f48f86ece0f9ca1c
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-06-24 23:15:07 +0300

    typeScript

--- 0.61 TS start/src/app17.ts
+++ 0.61 TS start/src/app17.ts
@@ -2,3 +2,11 @@
 // которой каждое слово заменено на последнюю букву этого слова, а все остальные
 // символы остаются без изменений.
 // "Hello World" -> "o d“
+const stroka: string | string[] = 'Hello Woord'.split(' ')
+let newStr: string = ''
+for (let i = 0; i < stroka.length; i++) {
+    let a = stroka[i][0];
+    let b = stroka[i][stroka[i].length - 1]
+    a = b;
+    console.log(a + stroka[i].slice(1));
+}

6db7f2ae0e8e5a2dada44cf3d01544a8c3cdc6bb
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-07-04 21:48:56 +0300

    typeScript

--- 0.65 TS principles of OOP/dist/app.js
+++ 0.65 TS principles of OOP/dist/app.js
@@ -3,45 +3,19 @@
 // реализует метод calculateArea() для расчета площади прямоугольника. Затем
 // создайте объект класса Rectangle и выведите его площадь на экран. (для
 // вычисления площади прямоугольника S = height * wigth)
-class StringManipulator {
-    constructor() {
-        this.str = 'TENET';
+class ServerGetAll {
+    controller() {
+        return this.service();
     }
-    reverseString() {
-        const reversed = this.str.split(' ').reverse().join(' ');
-        return reversed;
+    ;
+    service() {
+        return this.repository();
     }
-    isPalindrome() {
-        const reversed = this.str.split(' ').reverse().join(' ');
-        if (this.str === reversed) {
-            return 'палиндром';
-        }
-    }
-    countVowels() {
-        let count = 0;
-        for (let i = 0; i < this.str.length; i++) {
-            if (this.str[i].toLowerCase().includes("a" || "e" || "u" || "i" || "o")) {
-                count += 1;
-            }
-        }
-        return count;
+    ;
+    repository() {
+        const arr = [1, 2, 3];
+        return arr;
     }
 }
-const stringManipulator = new StringManipulator();
-console.log(stringManipulator.reverseString());
-console.log(stringManipulator.isPalindrome());
-console.log(stringManipulator.countVowels());
-// 8. Создайте класс PasswordGenerator, который будет иметь метод
-// generatePassword(length: number), позволяющий генерировать случайные
-// безопасные пароли заданной длины.
-// 9. Создать интерфейс iValidation. Создайте класс Validation, который содержит
-// приватные методы isValidEmail, isValidDate, isValidPath, вызывающиеся через
-// конструктор класса Validation. Конструктор принимает поля email, date, path и
-// инициализирует их как поля класса. В каждом методе напишите соответствующую
-// проверку.
-// 10. Создать интерфейс iPwd. Создайте класс Pwd. Pwd хранит функцию showPwd,
-// которая формирует и возвращает рандомно сгенерированный пароль из 8 чисел.
-// Класс Validation является производным по отношению к базовому, родительскому
-// Pwd и содержит функцию showPwd. Необходимо переопределить showPwd из
-// базового класса и записать в переменную используя метод super. Далее
-// дополняем переопределенный метод отображением результата в console
+const serverGetAll = new ServerGetAll();
+console.log(serverGetAll.controller());
--- 0.65 TS principles of OOP/src/app.ts
+++ 0.65 TS principles of OOP/src/app.ts
@@ -219,48 +219,96 @@
 // string), isPalindrome(str: string), countVowels(str: string). Реализуйте функционал для
 // разворота строки, проверки, является ли строка палиндромом, и подсчета
 // гласных букв в строке. Использовать Generics
-interface iStringManipulator {
-    str: string;
-    reverseString(): string;
-    isPalindrome(): string;
-    countVowels(): string;
-}
+// {
+//     interface iStringManipulator {
+//         str: string;
+//         reverseString(): string;
+//         isPalindrome(): string;
+//         countVowels(): string;
+//     }
 
-class StringManipulator {
-    str: string = 'TENET';
-    reverseString() {
-        const reversed = this.str.split(' ').reverse().join(' ')
-        return reversed
-    }
+//     class StringManipulator {
+//         str: string = 'TENET';
+//         reverseString() {
+//             const reversed = this.str.split(' ').reverse().join(' ')
+//             return reversed
+//         }
 
-    isPalindrome() {
-        const reversed = this.str.split(' ').reverse().join(' ')
-        if (this.str === reversed) {
-            return 'палиндром'
-        }
-    }
+//         isPalindrome() {
+//             const reversed = this.str.split(' ').reverse().join(' ')
+//             if (this.str === reversed) {
+//                 return 'палиндром'
+//             }
+//         }
 
-    countVowels() {
-        let count = 0;
-        for (let i = 0; i < this.str.length; i++) {
-            if (this.str[i].toLowerCase().includes("a" || "e" || "u" || "i" || "o")) {
-                count += 1
-            }
-        }
-        return count
-    }
-}
+//         countVowels() {
+//             let count = 0;
+//             for (let i = 0; i < this.str.length; i++) {
+//                 if (this.str[i].toLowerCase().includes("a" || "e" || "u" || "i" || "o")) {
+//                     count += 1
+//                 }
+//             }
+//             return count
+//         }
+//     }
+//     const stringManipulator = new StringManipulator();
+//     console.log(stringManipulator.reverseString());
+//     console.log(stringManipulator.isPalindrome());
+//     console.log(stringManipulator.countVowels());
+// }
 
-const stringManipulator = new StringManipulator();
-console.log(stringManipulator.reverseString());
-console.log(stringManipulator.isPalindrome());
-console.log(stringManipulator.countVowels());
+// ||
+
+// {class StringManipulator {
+//     reverseString<MyType>(str: MyType): void {
+//         if (typeof (str) === 'string')
+//             console.log(str.split(' ').reverse().join(' '));
+//     }
+
+//     isPalindrome<MyType>(str: MyType): void {
+//         if (typeof (str) === 'string')
+//             console.log(str === str.split(' ').reverse().join(' ') ? true : false)
+//     }
+
+//     countVowels<MyType>(str: MyType) {
+//         if (typeof (str) === 'string')
+//             const word: string = 'eyioa';
+//         let count: number = 0;
+//         for (let i = 0; i < str.length; i++) {
+//             if (str.includes(word[i])) {
+//                 count += 1
+//             }
+//         }
+//         return count
+//     }
+// }
+
+// const stringManipulator = new StringManipulator();
+// console.log(stringManipulator.reverseString<string>('палиндром'));
+// console.log(stringManipulator.isPalindrome<string>('анна'));
+// console.log(stringManipulator.countVowels<string>('sgfhgqwuoeiyppo'));}
 
 // 8. Создайте класс PasswordGenerator, который будет иметь метод
 // generatePassword(length: number), позволяющий генерировать случайные
 // безопасные пароли заданной длины.
 
+// {
+//     class PasswordGenerator {
+//         generatePassword<MyType>(pwdLength: MyType): string {
+//             if (typeof (pwdLength) === "number") {
+//                 const symbols: string = 'qwertyuiopasdfghjklzxcvbnm,./1234567890-=!@#$%^&&*()_+='
+//                 let pwd: string = ''
+//                 for (let i = 0; i < pwdLength; i++) {
+//                     pwd += symbols[Math.round(Math.random() * symbols.length)]
+//                 }
+//                 return pwd
+//             }
+//         }
+//     }
 
+//     const passwordGenerator = new PasswordGenerator();
+//     console.log(passwordGenerator.generatePassword<number>(10))
+// }
 
 // 9. Создать интерфейс iValidation. Создайте класс Validation, который содержит
 // приватные методы isValidEmail, isValidDate, isValidPath, вызывающиеся через
@@ -268,7 +316,45 @@ console.log(stringManipulator.countVowels());
 // инициализирует их как поля класса. В каждом методе напишите соответствующую
 // проверку.
 
+// {interface iValidation {
+//     email: string;
+//     date: string;
+//     path: string;
+// }
 
+// class Validation implements iValidation{
+//     email: string;
+//     date: string;
+//     path: string;
+
+//     constructor(email: string, date: string, path: string) {
+//         this.email = email;
+//         this.date = date;
+//         this.path = path;
+
+//         this.isValidEmail();
+//         this.isValidDate();
+//         this.isValidPath()
+//     }
+
+//     private isValidEmail(): void {
+//         (/^[a-zA-z0-9\_]+\@[mail]+\.[a-z]{2}$/gm.test(this.email)) ?   console.log('хароший пароль' : 'плохой пароль');
+//     };
+
+//     private isValidDate(): void {
+//         if (/^[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}$/gm.test(this.date)) {
+//             console.log('good');
+//         }
+//     }
+
+//     private isValidPath(): void {
+//         if (/^[A-Z]{1}\:\\\\[a-zA-Z0-9\\а-яА-Я \.]+\.[a-z]{2,4}/gm.test(this.date)) {
+//             console.log('good');
+//         }
+//     }
+// }
+
+// const validation: iValidation = new Validation('vikynyshka@mail.ru', '15.04.17', 'C:\Users\Dev\OneDrive\Рабочий стол\daily\Templates.typescript\app.js');}
 
 // 10. Создать интерфейс iPwd. Создайте класс Pwd. Pwd хранит функцию showPwd,
 // которая формирует и возвращает рандомно сгенерированный пароль из 8 чисел.
@@ -276,3 +362,34 @@ console.log(stringManipulator.countVowels());
 // Pwd и содержит функцию showPwd. Необходимо переопределить showPwd из
 // базового класса и записать в переменную используя метод super. Далее
 // дополняем переопределенный метод отображением результата в console
+
+
+// 12. Реализуйте класс ServerGetAll. Обязательными функциями считаются функции
+// controller, service, repository. Цепочка взаимодействия между методами
+// следующая: controller -> service -> repository, где:
+// Задание:
+// Необходимо вывести в консоль массив из репозитория
+
+// {interface iServerGetAll{
+//     controller():number[];
+//     service():number[];
+//     repository():number[]
+// }
+
+// class ServerGetAll implements iServerGetAll {
+//     controller():number[] {
+//         return this.service()
+//     };
+
+//     service():number[] {
+//         return this.repository()
+//     };
+
+//     repository():number[] {
+//         const arr:number[] = [1, 2, 3]
+//         return arr
+//     }
+// }
+
+// const serverGetAll = new ServerGetAll(); 
+// console.log(serverGetAll.controller());}
